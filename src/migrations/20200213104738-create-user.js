fd06080c709e456e07787bbf8e06c54490924fba
aidooyaw1992 <aidooyaw1992@gmail.com>
2020-02-20 10:37:59 +0000

    added promise to users migration

--- src/migrations/20200213104738-create-user.js
+++ src/migrations/20200213104738-create-user.js
@@ -1,47 +1,55 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      firstName: {
-        type: Sequelize.STRING
-      },
-      lastName: {
-        type: Sequelize.STRING
-      },
-      position: {
-        type: Sequelize.STRING
-      },
-      company: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING,
-        unique: true
-      },
-      password: {
-        type: Sequelize.STRING,
-        unique: true
-      },
-      username: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    return Promise.all([
+      queryInterface.createTable('Users', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        firstName: {
+          type: Sequelize.STRING
+        },
+        lastName: {
+          type: Sequelize.STRING
+        },
+        position: {
+          type: Sequelize.STRING
+        },
+        company: {
+          type: Sequelize.STRING
+        },
+        email: {
+          type: Sequelize.STRING,
+          unique: true
+        },
+        password: {
+          type: Sequelize.STRING,
+          unique: true
+        },
+        username: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true
+        },
+        isAdmin: {
+          type: Sequelize.BOOLEAN,
+          defaultValue: false
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      }),
+
+
+    ]);
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Users');
