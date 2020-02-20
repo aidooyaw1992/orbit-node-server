5cb402fe65d6bc8633fe566fe7bcac4714d50147
aidooyaw1992 <aidooyaw1992@gmail.com>
2020-02-20 10:37:36 +0000

    updated attendees table

--- src/migrations/20200213112135-create-attendee.js
+++ src/migrations/20200213112135-create-attendee.js
@@ -11,7 +11,22 @@ module.exports = {
           primaryKey: true,
           type: Sequelize.INTEGER
         },
+        title: {
+          type: Sequelize.STRING
+        },
+        firstName: {
+          type: Sequelize.STRING
+        },
+        lastName: {
+          type: Sequelize.STRING
+        },
+        otherName: {
+          type: Sequelize.STRING
+        },
         fullName: {
+          type: Sequelize.STRING
+        },
+        nationality: {
           type: Sequelize.STRING
         },
         email: {
@@ -26,15 +41,32 @@ module.exports = {
         organization: {
           type: Sequelize.STRING
         },
+        amount: Sequelize.REAL,
+        heardAboutUs: {
+          type: Sequelize.TEXT
+        },
+        CountryOfResidence: {
+          type: Sequelize.STRING
+        },
         preCode: {
           type: Sequelize.STRING
         },
         eventId: {
           type: Sequelize.INTEGER
         },
+        ownerId: {
+          type: Sequelize.INTEGER
+        },
         isVerified: {
           type: Sequelize.BOOLEAN,
           defaultValue: false
+        },
+        paid: {
+          type: Sequelize.BOOLEAN,
+          defaultValue: false
+        },
+        macAddress: {
+          type: Sequelize.STRING
         },
         dateAttended1: Sequelize.DATE,
         dateAttended2: Sequelize.DATE,
