a341ff81a737fa294696cc2f5007136536ec53de
aidooyaw1992 <aidooyaw1992@gmail.com>
2020-02-27 12:46:01 +0000

    added clear image and add image api function

--- routes/eventsRoute.js
+++ routes/eventsRoute.js
@@ -1,7 +1,11 @@
 const Express = require('express');
 var model = require('../models/index');
+const uploader = require('../handlers/multer');
 const Joi = require('joi');
 const router = Express.Router();
+const cloudinary = require('../handlers/cloudinary');
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', (req, res)=>{
     model.Event.findAll({
@@ -16,8 +20,49 @@ router.get('/', (req, res)=>{
         res.status(404).json({
             error: err
         });
-    }
-    );
+    });
+});
+
+router.get('/clear_images', (req, res) =>{
+    let imagePath=path.join(__dirname,'../uploads/');
+    fs.readdir(imagePath, function(err, files) {
+        if (err) {
+            console.log("Error getting directory information.")
+        } else {
+        files.forEach(function(file) {
+            console.log(file);
+            let imgPath = path.join(imagePath, file);
+            fs.unlinkSync(imgPath,function (err) {
+                if (err) throw err;
+                console.log('File deleted!');
+            })
+          })
+        }
+      });
+      return;
+});
+
+router.post('/add_image', uploader.single('image'), (req, res) =>{
+    console.log(req.file);
+    cloudinary.uploader.upload(req.file.path)
+    .then(uploadRes => {
+        let imagePath=path.join(__dirname,'../uploads/');
+        fs.readdir(imagePath, (err, files) =>{
+            if (err) {
+                console.log("Error getting directory information.");
+                return res.send(err);
+            }else{
+                let file = files[0];
+                console.log(file);
+                let imgPath = path.join(imagePath, file);
+                fs.unlinkSync(imgPath,function (err) {
+                    if (err) return res.send(err);
+                    console.log('File deleted!');
+                })
+            }
+        })
+        res.status(200).send(uploadRes);
+    }).catch(err => res.status(400).send(err));
 });
 
 router.post('/add_event', (req, res) =>{
