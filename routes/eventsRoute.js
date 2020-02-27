d20df0dd5cf5f4365f34980d621988981df01282
aidooyaw1992 <aidooyaw1992@gmail.com>
2020-02-27 14:04:42 +0000

    updated add_image api function

--- routes/eventsRoute.js
+++ routes/eventsRoute.js
@@ -33,38 +33,53 @@ router.get('/clear_images', (req, res) =>{
         files.forEach(function(file) {
             console.log(file);
             let imgPath = path.join(imagePath, file);
-            fs.unlinkSync(imgPath,function (err) {
+            fs.unlinkSync(imgPath, (err) => {
                 if (err) throw err;
                 console.log('File deleted!');
             })
-          })
+        })
         }
-      });
-      return;
+    });
+    return;
 });
 
-router.post('/add_image', uploader.single('image'), (req, res) =>{
-    console.log(req.file);
-    cloudinary.uploader.upload(req.file.path)
-    .then(uploadRes => {
-        let imagePath=path.join(__dirname,'../uploads/');
-        fs.readdir(imagePath, (err, files) =>{
-            if (err) {
-                console.log("Error getting directory information.");
-                return res.send(err);
-            }else{
-                let file = files[0];
-                console.log(file);
-                let imgPath = path.join(imagePath, file);
-                fs.unlinkSync(imgPath,function (err) {
-                    if (err) return res.send(err);
-                    console.log('File deleted!');
-                })
-            }
-        })
-        res.status(200).send(uploadRes);
-    }).catch(err => res.status(400).send(err));
+
+router.post('/add_image', multerUploads,(req,res) =>{
+    if(req.file){
+        let file = dataUri(req).content;
+        return cloudinary.uploader.upload(file).then(result => {
+            const image = result.url;
+            console.log(image);
+            return res.status(200).json({
+                message:'saved succesfully',
+                data: result
+            })
+        }).catch(err => res.status(400).json(err));
+    }
 });
+
+// router.post('/add_image', uploader.single('image'), (req, res) =>{
+//     console.log(req.file);
+//     cloudinary.uploader.upload(req.file.path)
+//     .then(uploadRes => {
+//         let imagePath=path.join(__dirname,'../uploads/');
+//         fs.readdir(imagePath, (err, files) =>{
+//             if (err) {
+//                 console.log("Error getting directory information.");
+//                 return res.send(err);
+//             }else{
+//                 let file = files[0];
+//                 console.log(file);
+//                 let imgPath = path.join(imagePath, file);
+//                 fs.unlinkSync(imgPath,function (err) {
+//                     if (err) return res.send(err);
+//                     console.log('File deleted!');
+//                 })
+//             }
+//         })
+//         res.status(200).send(uploadRes);
+//     }).catch(err => res.status(400).send(err));
+// });
 
 router.post('/add_event', (req, res) =>{
 
